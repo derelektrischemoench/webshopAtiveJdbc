2ed5c2c34dd86fc7a95fba1c6735eac451c1c395
Chris Bader <baederchris@gmail.com>
2018-03-06 21:01:47 +0100

    fileupload wip

--- src/main/java/servlet/AddRecord.java
+++ src/main/java/servlet/AddRecord.java
@@ -1,21 +1,47 @@
 package servlet;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.output.*;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
-        maxFileSize = 1024 * 1024 * 10,      // 10MB
+        maxFileSize = 1024 * 1024 * 20,      // 20MB
         maxRequestSize = 1024 * 1024 * 50)   // 50MB
+
 public class AddRecord extends HttpServlet {
-    //http://www.codejava.net/java-ee/servlet/java-file-upload-example-with-servlet-30-api
-    private static final String UPLOAD_PATH = "uploaded_files";
+    
+    private static final long serialVersionUID = 1L;
+    private ServletFileUpload uploader = null;
+    
+    @Override
+	public void init() throws ServletException{
+		DiskFileItemFactory fileFactory = new DiskFileItemFactory();
+		File filesDir = (File) getServletContext().getAttribute("FILES_DIR_FILE");
+		fileFactory.setRepository(filesDir);
+		this.uploader = new ServletFileUpload(fileFactory);
+	}
+    
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,46 +50,38 @@ public class AddRecord extends HttpServlet {
         rd.forward(req, resp);
     }
     
-    //todo: fix file upload
-    @Override
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("doPost in addRecord");
-        // gets absolute path of the web application
-        String appPath = request.getRealPath("/");
-        System.out.println("apppath: " + appPath);
-        // constructs path of the directory to save uploaded file
-        String savePath = appPath + File.separator + UPLOAD_PATH;
-        
-        // creates the save directory if it does not exists
-        File fileSaveDir = new File(savePath);
-        if (!fileSaveDir.exists()) {
-            fileSaveDir.mkdir();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            throw new ServletException("Content type is not multipart/form-data");
         }
         
-        for (Part part : request.getParts()) {
-            String fileName = extractFileName(part);
-            // refines the fileName in case it is an absolute path
-            fileName = new File(fileName).getName();
-            part.write(savePath + File.separator + fileName);
-        }
-        request.setAttribute("message", "Upload has been done successfully!");
-        getServletContext().getRequestDispatcher("/message.jsp").forward(
-                request, response);
-    }
-    
-    /**
-     * Extracts file name from HTTP header content-disposition
-     */
-    private String extractFileName(Part part) {
-        String contentDisp = part.getHeader("content-disposition");
-        String[] items = contentDisp.split(";");
-        for (String s : items) {
-            if (s.trim().startsWith("filename")) {
-                return s.substring(s.indexOf("=") + 2, s.length() - 1);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.write("<html><head></head><body>");
+        try {
+            List<FileItem> fileItemsList = uploader.parseRequest(request);
+            Iterator<FileItem> fileItemsIterator = fileItemsList.iterator();
+            while (fileItemsIterator.hasNext()) {
+                FileItem fileItem = fileItemsIterator.next();
+                System.out.println("FieldName=" + fileItem.getFieldName());
+                System.out.println("FileName=" + fileItem.getName());
+                System.out.println("ContentType=" + fileItem.getContentType());
+                System.out.println("Size in bytes=" + fileItem.getSize());
+                
+                File file = new File("imgs" + File.separator + fileItem.getName());
+                System.out.println("Absolute Path at server=" + file.getAbsolutePath());
+                fileItem.write(file);
+                out.write("File " + fileItem.getName() + " uploaded successfully.");
+                out.write("<br>");
+                out.write("<a href=\"UploadDownloadFileServlet?fileName=" + fileItem.getName() + "\">Download " + fileItem.getName() + "</a>");
             }
+        } catch (FileUploadException e) {
+            out.write("Exception in uploading file.");
+            out.write(e.getMessage());
+        } catch (Exception e) {
+            out.write("Exception in uploading file.");
         }
-        return "";
+        out.write("</body></html>");
     }
     
     
--- src/main/webapp/addRecord.jsp
+++ src/main/webapp/addRecord.jsp
@@ -11,7 +11,7 @@
     <div class="row">
         <div class="col-sm-8 offset-sm-2">
             <form action="<c:out value="${pageContext.request.contextPath}/adminLogin/addRecord" />" method="post" enctype="multipart/form-data">
-                <div class="form-group">
+                <!--<div class="form-group">
                     <label for="recordName">Plattenname</label>
                     <input type="text" class="form-control" id="recordName" aria-describedby="emailHelp"
                            placeholder="Enter the name of the record" name="createRecord__recordName">
@@ -21,7 +21,7 @@
                     <label for="artistName">Künstler</label>
                     <input type="text" class="form-control" id="artistName" placeholder="Enter the Artist's name here"
                            name="createRecord__artistName">
-                </div>
+                </div>-->
 
                 <div class="form-group">
                     <label for="recordImage">Upload record image</label><br>
