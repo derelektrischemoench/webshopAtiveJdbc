03db128d581ebdf3f68c09576e31fbc06d67c18a
Chris Bader <baederchris@gmail.com>
2018-03-08 21:11:43 +0100

    wip des todes of doom trying to bytearray and base64 this fucking file

--- src/main/java/servlet/adminServlets/AddArtist.java
+++ src/main/java/servlet/adminServlets/AddArtist.java
@@ -1,7 +1,14 @@
 package servlet.adminServlets;
 
 import model.Artist;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.bouncycastle.util.encoders.Base64;
 import org.javalite.activejdbc.Base;
+import org.javalite.http.Multipart;
+import org.sqlite.util.StringUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,11 +17,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 //TODO: NOTE: files are save in domains/domain1/generated/jsp/webshop
 
@@ -35,39 +48,68 @@ public class AddArtist extends HttpServlet {
         //open database connection:
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/wpr_webshop", "root", "root");
         
-        String savePath = "/";
-        //IMAGE SHIT
-        //create fileDir if !exists:
-        File uploadedImage = new File(savePath);
-        String filename = null;
-        
-        //we can get the single image like so:
-        //todo: optimize this below. by getting only the image part of the request. right now it contains the empty form fields as well and creates an empty file for them
-       
-        for (Part part : request.getParts()) {
-            //also add an upload timestamp:
-            String timestamp =  new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-            //check which part is named like the image and thenwrite this
-            //apparently the image cant be written without the other part of the request for fucks sake
-            if(part.getName().contains("Image")) {
-                filename = timestamp + part.getSubmittedFileName().replace(" ", "");
-                part.write(filename);
+        try {
+            List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+            
+            Iterator fileItemIterators = multiparts.iterator();
+            while (fileItemIterators.hasNext()) {
+                Part p = (Part) fileItemIterators.next();
+                System.out.println("request item: " + p.getSubmittedFileName());
+                System.out.println("request item name: " + p.getName());
             }
             
+            Part file = request.getPart("createArtist__artistImage"); // this is the file
+            System.out.println("file attrs: " + file.getSubmittedFileName() + " " + file.getSize());
+    
+//FFFFUUUUUUCK
+            //CREATE ARTIST, save to db, yada yada
+            Artist.createIt("artist_name", artistName,
+                            "first_name", artistFirstName,
+                            "last_name", artistLastName,
+                            "label", "placeholder in artistCreateIt in AddArtist",
+                            "artist_img_path", "asdasd");
+            
+            Base.close();
+            request.setAttribute("artistName", artistName);
+            RequestDispatcher rd = request.getRequestDispatcher("/artistSuccessfullyAdded.jsp");
+            rd.forward(request, resp);
+            
+        } catch (FileUploadException e) {
+            e.printStackTrace();
         }
         
-        //CREATE ARTIST, save to db, yada yada
-        Artist.createIt("artist_name", artistName,
-                        "first_name", artistFirstName,
-                        "last_name", artistLastName,
-                        "label", "placeholder in artistCreateIt in AddArtist",
-                        "artist_img_path", filename);
         
-        Base.close();
-        request.setAttribute("artistName", artistName);
-        RequestDispatcher rd = request.getRequestDispatcher("/artistSuccessfullyAdded.jsp");
-        rd.forward(request, resp);
     }
-
+    
+    public void saveImage(InputStream input, File file) {
+        try {
+            
+            OutputStream output = null;
+            try {
+                output = new FileOutputStream(file);
+            } catch (FileNotFoundException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            try {
+                try {
+                    final byte[] buffer = new byte[1024];
+                    int read;
+                    
+                    while ((read = input.read(buffer)) != -1)
+                        output.write(buffer, 0, read);
+                    
+                    output.flush();
+                } finally {
+                    output.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } finally {
+        
+        }
+    }
+    
 }
 
