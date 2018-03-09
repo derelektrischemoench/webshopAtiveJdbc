82bcf5c40d6f603b1d0469508fcd20e8a6398ea2
Chris Bader <baederchris@gmail.com>
2018-03-09 18:20:06 +0100

    got the fucking fileupload working

--- src/main/java/servlet/adminServlets/AddArtist.java
+++ src/main/java/servlet/adminServlets/AddArtist.java
@@ -1,115 +1,95 @@
 package servlet.adminServlets;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
-import model.Artist;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.bouncycastle.util.encoders.Base64;
-import org.javalite.activejdbc.Base;
-import org.javalite.http.Multipart;
-import org.sqlite.util.StringUtils;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
-import java.io.*;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.Path;
-
-//TODO: NOTE: files are save in domains/domain1/generated/jsp/webshop
-
-@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, maxFileSize = 1024 * 1024 * 10, maxRequestSize = 1024 * 1024 * 50)
+/**
+ * Servlet implementation class UploadServlet
+ */
 public class AddArtist extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static final String DATA_DIRECTORY = "WEB-INF/data";
+    private static final int MAX_MEMORY_SIZE = 1024 * 1024 * 2;
+    private static final int MAX_REQUEST_SIZE = 1024 * 1024;
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher rd = req.getRequestDispatcher("/addArtist.jsp");
         rd.forward(req, resp);
     }
     
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doPost in addArtist");
-        String artistName = request.getParameter("createArtist__artistName");
-        String artistFirstName = request.getParameter("createArtist__artistFirstName");
-        String artistLastName = request.getParameter("createArtist__artistLastName");
-        //open database connection:
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/wpr_webshop", "root", "root");
-        
-        try {
-            List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
-            
-            Iterator fileItemIterators = multiparts.iterator();
-            while (fileItemIterators.hasNext()) {
-                Part p = (Part) fileItemIterators.next();
-                System.out.println("request item: " + p.getSubmittedFileName());
-                System.out.println("request item name: " + p.getName());
-            }
-            
-            Part file = request.getPart("createArtist__artistImage"); // this is the file
-            System.out.println("file attrs: " + file.getSubmittedFileName() + " " + file.getSize());
-    
-//FFFFUUUUUUCK
-            //CREATE ARTIST, save to db, yada yada
-            Artist.createIt("artist_name", artistName,
-                            "first_name", artistFirstName,
-                            "last_name", artistLastName,
-                            "label", "placeholder in artistCreateIt in AddArtist",
-                            "artist_img_path", "asdasd");
-            
-            Base.close();
-            request.setAttribute("artistName", artistName);
-            RequestDispatcher rd = request.getRequestDispatcher("/artistSuccessfullyAdded.jsp");
-            rd.forward(request, resp);
-            
-        } catch (FileUploadException e) {
-            e.printStackTrace();
-        }
-        
-        
-    }
-    
-    public void saveImage(InputStream input, File file) {
-        try {
-            
-            OutputStream output = null;
-            try {
-                output = new FileOutputStream(file);
-            } catch (FileNotFoundException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-            try {
-                try {
-                    final byte[] buffer = new byte[1024];
-                    int read;
-                    
-                    while ((read = input.read(buffer)) != -1)
-                        output.write(buffer, 0, read);
-                    
-                    output.flush();
-                } finally {
-                    output.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } finally {
-        
-        }
-    }
-    
-}
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Check that we have a file upload request
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+        if (!isMultipart) {
+            return;
+        }
+
+        // Create a factory for disk-based file items
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        // Sets the size threshold beyond which files are written directly to
+        // disk.
+        factory.setSizeThreshold(MAX_MEMORY_SIZE);
+
+        // Sets the directory used to temporarily store files that are larger
+        // than the configured size threshold. We use temporary directory for
+        // java
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+        // constructs the folder where uploaded file will be stored
+        String uploadFolder = getServletContext().getRealPath("")
+                + File.separator + DATA_DIRECTORY;
+
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        // Set overall request size constraint
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+
+        try {
+            // Parse the request
+            List items = upload.parseRequest(request);
+            Iterator iter = items.iterator();
+            while (iter.hasNext()) {
+                FileItem item = (FileItem) iter.next();
+
+                if (!item.isFormField()) {
+                    String fileName = new File(item.getName()).getName();
+                    String filePath = uploadFolder + File.separator + fileName;
+                    File uploadedFile = new File(filePath);
+                    System.out.println(filePath);
+                    // saves the file to upload directory
+                    item.write(uploadedFile);
+                }
+            }
+
+            // displays done.jsp page after upload finished
+            getServletContext().getRequestDispatcher("/done.jsp").forward(
+                    request, response);
+
+        } catch (FileUploadException ex) {
+            throw new ServletException(ex);
+        } catch (Exception ex) {
+            throw new ServletException(ex);
+        }
+
+    }
+
+}
--- src/main/java/servlet/fileUploadService/FileUploadService.java
+++
@@ -1,5 +0,0 @@
-package servlet.fileUploadService;
-
-public class FileUploadService {
-    //TODO: https://javatutorial.net/java-file-upload-rest-service
-}
