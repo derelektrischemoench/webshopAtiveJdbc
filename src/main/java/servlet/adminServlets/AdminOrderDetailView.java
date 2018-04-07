a133b9050c99ca9001f2e693b6c51f2e6ce85991
Chris Bader <baederchris@gmail.com>
2018-04-07 10:11:59 +0200

    yey k:V mappings for records in order and their amount

--- src/main/java/servlet/adminServlets/AdminOrderDetailView.java
+++ src/main/java/servlet/adminServlets/AdminOrderDetailView.java
@@ -2,6 +2,7 @@ package servlet.adminServlets;
 
 import model.Order;
 import model.Record;
+import model.RecordsShoppingcarts;
 import model.Shoppingcart;
 
 import javax.servlet.RequestDispatcher;
@@ -10,20 +11,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class AdminOrderDetailView extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int orderId = Integer.parseInt(req.getParameter("orderId"));
         Order o = Order.findById(orderId);
-    
         Shoppingcart s = Shoppingcart.findById(o.getInteger("shoppingcart_id"));
-        List<Record> records = s.getAll(Record.class);
-        System.out.println("numrecords in ordder: " + records.size());
+        String shoppingCartId = s.getString("id");
+        List <RecordsShoppingcarts> rs = RecordsShoppingcarts.where("shoppingcart_id = ?", shoppingCartId);
+    
+        HashMap<Record, Integer> recordAndAmount = new HashMap<>();
+    
+        Iterator rsIter = rs.iterator();
+        while (rsIter.hasNext()) {
+            RecordsShoppingcarts r = (RecordsShoppingcarts)rsIter.next();
+            Record rec = Record.findById(r.getInteger("record_id"));
+            int amount = r.getInteger("record_amount");
+            recordAndAmount.put(rec, amount);
+        }
+    
+        System.out.println("verify the hashmap:");
+        Iterator<Map.Entry<Record, Integer>> iter = recordAndAmount.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Record, Integer> entrySet = iter.next();
+            System.out.println("record: " + entrySet.getKey().getString("title") + "amount: " + entrySet.getValue());
+        }
         
         req.setAttribute("order", o);
-        req.setAttribute("records", records);
+        req.setAttribute("recordAndAmount", recordAndAmount);
         
         
         RequestDispatcher rd = req.getRequestDispatcher("/adminOrderDetail.jsp");
